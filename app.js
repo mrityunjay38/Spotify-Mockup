5d8d1d1d2fa068571e0de21d6f1a7797595a6379
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-09-27 14:55:04 +0530

    Adds database connectivity

--- app.js
+++ app.js
@@ -3,8 +3,48 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+let members = [
+    {
+        name : "MJ",
+        email : '@gmail.com',
+    },
+    {
+        name : "CJ",
+        email : '@mail.com',
+    },
+    {
+        name : "DJ",
+        email : '@yahoo.com',
+    }
+];
+
 app.get('/api/members', (req, res) => {
+    return res.json(members);
 });
+
+app.post('/api/members', (req, res) => {
+    const newMember = {
+        name : req.body.name,
+        email : req.body.email
+    }
+    members.push(newMember);
+    res.json(members);
+});
+
+app.put('/api/members', (req, res) => {
+    members[req.body.index].email = req.body.email
+    res.json(members);
+} );
+
+app.delete('/api/members', (req, res) => {
+    members.pop();
+    res.json(members);
+});
+
+
 
 // Set static folder
 
--- database/connect.js
+++ database/connect.js
@@ -1,15 +1,39 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+// Database credentials
+
+const loginDetails = {
     host : 'localhost',
     user : 'mj',
     password : '12',
     database : 'spotify'
+}
+
+// Establish connection
+
+const database = mysql.createConnection(loginDetails);
+
+database.connect( err => {
+    if(err){
+        throw(err);
+    }
+    console.log('MySql Database connected...');
 });
 
-db.connect( err => {
-    if(err){
-        throw err;
-    }
-    console.log('MySql connected....');
-});
+// database.end();
+
+// Promisify query
+
+function dbQuery(query, val) {
+    return new Promise ( (resolve,reject) => {
+        database.query(query, val, (err,result) => {
+            if(err)
+                reject(err);
+            else 
+                resolve(result);
+        })
+    })
+}
+
+module.exports = dbQuery;
+
